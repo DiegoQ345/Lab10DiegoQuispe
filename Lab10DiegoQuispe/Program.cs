00f09db9816fc82cd580f5daf4c77ffef0dec949
Diego Zahid Quispe Quispe <diego.quispe.qui@tecsup.edu.pe>
2026-06-15 15:49:25 -0500

    Update Conection String

--- Lab10DiegoQuispe/Program.cs
+++ Lab10DiegoQuispe/Program.cs
@@ -1,11 +1,20 @@
 using Hangfire;
 using Lab10DiegoQuispe.Configurations;
+using Lab10DiegoQuispe.Persistence.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
+
+// Aplicar migraciones automáticamente
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
